2b42759e512995b4e87c10f9be65451e39e1e7a3
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 10:58:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
 function forLoop(arr){
-  for(var i=0;i<25;i++) arr.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+  for(var i=0;i<25;i++){
+    arr.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+  }
 }
